24473adaef76d9f9629b8efc721f20d1189ca8a6
David <wilsonfox655@gmail.com>
2023-05-23 22:48:31 +0400

    Added code to prompt the user for two numbers and calculate their sum

--- test_code.cpp
+++ test_code.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
-    cout << "Hello, World!" << endl;
+    cout << "Calculator" << endl;
+
+    int num1, num2;
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    int sum = num1 + num2;
+    cout << "Sum: " << sum << endl;
 
     return 0;
 }
